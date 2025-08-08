708b0420ccb82de8005c7ee2488abedc21493bbd
Dynamic-Capital <deathcoder8@gmail.com>
2025-08-08 17:56:12 +0500

    chore: satisfy deno lint

--- types/tesseract.d.ts
+++ types/tesseract.d.ts
@@ -1,9 +1,19 @@
-declare module "tesseract.js" {
-  export function createWorker(): any;
-  export type Worker = any;
+interface BaseWorker {
+  load(): Promise<void>;
+  loadLanguage(lang: string): Promise<void>;
+  initialize?(lang: string): Promise<void>;
+  reinitialize?(lang: string): Promise<void>;
+  setParameters(params: Record<string, string>): Promise<void>;
+  recognize(blob: Blob): Promise<{ data: { text: string } }>;
+  terminate(): Promise<void>;
 }
 
 declare module "npm:tesseract.js@5" {
-  export function createWorker(): any;
-  export type Worker = any;
+  export function createWorker(): Promise<BaseWorker>;
+  export type Worker = BaseWorker;
+}
+
+declare module "tesseract.js" {
+  export function createWorker(): Promise<BaseWorker>;
+  export type Worker = BaseWorker;
 }
