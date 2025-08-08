3aef513a6c63f0c8c57a717a40e32ad992f3c2cf
Dynamic-Capital <deathcoder8@gmail.com>
2025-08-08 17:44:38 +0500

    refactor: tighten OCR worker typings

--- types/tesseract.d.ts
+++ types/tesseract.d.ts
@@ -1,3 +1,9 @@
 declare module "tesseract.js" {
   export function createWorker(): any;
+  export type Worker = any;
+}
+
+declare module "npm:tesseract.js@5" {
+  export function createWorker(): any;
+  export type Worker = any;
 }
