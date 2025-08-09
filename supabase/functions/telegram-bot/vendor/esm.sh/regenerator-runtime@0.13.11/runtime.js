f82a0604f7d0861a1e741f94ef5f91269d865b71
Dynamic-Capital <deathcoder8@gmail.com>
2025-08-09 11:43:21 +0500

    chore: remove ocr dependencies

--- supabase/functions/telegram-bot/vendor/esm.sh/regenerator-runtime@0.13.11/runtime.js
+++
@@ -1,3 +0,0 @@
-// @deno-types="https://esm.sh/@types/regenerator-runtime@~0.13.8/index.d.ts"
-export * from "./runtime.proxied.js";
-export { default } from "./runtime.proxied.js";
--- supabase/functions/telegram-bot/vendor/esm.sh/tesseract.js@5.1.1.js
+++
@@ -1,3 +0,0 @@
-// @deno-types="../../../../../../types/tesseract.d.ts"
-export * from "./tesseract.js@5.1.1.proxied.js";
-export { default } from "./tesseract.js@5.1.1.proxied.js";
