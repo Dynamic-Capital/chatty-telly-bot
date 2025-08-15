6c23c3412931dbeb61e702d83c41c133577df1e9
Dynamic-Capital <deathcoder8@gmail.com>
2025-08-15 07:19:11 +0500

    Move mini app into supabase functions directory

--- supabase/functions/miniapp/vite.config.ts
+++ supabase/functions/miniapp/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../../supabase/functions/miniapp/static',
+    outDir: './static',
     emptyOutDir: true,
     rollupOptions: {
       output: {
